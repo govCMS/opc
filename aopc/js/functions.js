8ab8c1ad2d8d15adcb9d1db69e554e73f7b716e8
Raushan Mustafin <RMustafin@opc.com.au>
2016-10-21 15:58:59 +1100

    AOPC Drupal theme files and its parent theme - bootstrap

---
+++ aopc/js/functions.js
@@ -0,0 +1,6 @@
+jQuery( document ).ready(function($) {
+    $('.view-id-landing_page .views-row').matchHeight();
+    
+    $('#search-block-form button').attr('title','Search');
+    $('#search-block-form #edit-actions').replaceWith('');
+});
---
+++ aopc/template.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * The primary PHP file for this theme.
+ */
+
+function aopc_js_alter(&$javascript) {
+  $node_admin_paths = array(
+    'node/*/edit',
+    'node/add',
+    'node/add/*',
+  );
+  $replace_jquery = TRUE;
+  if (path_is_admin(current_path())) {
+    $replace_jquery = FALSE;
+  }
+  else {
+    foreach ($node_admin_paths as $node_admin_path) {
+      if (drupal_match_path(current_path(), $node_admin_path)) {
+        $replace_jquery = FALSE;
+      }
+    }
+  }
+  // Swap out jQuery to use an updated version of the library.
+  if ($replace_jquery) {
+    $javascript['misc/jquery.js']['data'] = '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js';
+  }
+}
